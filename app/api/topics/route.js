ac27d2e53fe8e103f6cf7c1c8d8f0c8b10ee0e54
ThomasMeyer <tmeyerwebdev@gmail.com>
2023-12-04 15:15:35 -0800

    adds basic crud operation and connects to mongodb

---
+++ app/api/topics/route.js
@@ -0,0 +1,33 @@
+import connectMongoDB from "@/libs/mongodb";
+import Topic from "@/models/topic";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  const { title, description } = await request.json();
+  await connectMongoDB();
+
+  // Check if a topic with the same title already exists
+  const existingTopic = await Topic.findOne({ title });
+
+  if (existingTopic) {
+    // If a topic with the same title exists, return an error response
+    return NextResponse.json({ message: "Topic with the same title already exists" }, { status: 400 });
+  }
+
+
+  await Topic.create({ title, description });
+  return NextResponse.json({ message: "Topic Created" }, { status: 201 });
+}
+
+export async function GET() {
+  await connectMongoDB();
+  const topics = await Topic.find();
+  return NextResponse.json({ topics });
+}
+
+export async function DELETE(request) {
+  const id = request.nextUrl.searchParams.get("id");
+  await connectMongoDB();
+  await Topic.findByIdAndDelete(id);
+  return NextResponse.json({ message: "Topic deleted" }, { status: 200 });
+}
--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} suppressHydrationWarning={true}>
         <div className='max-w-3xl mx-auto p-4'>
           <Navbar />
           <div className='mt-8'>
